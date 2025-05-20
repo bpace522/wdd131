77365b5a3a06e7e98c0bd41868462d169665413f
lionenergyautomation <automation@lionenergy.com>
2025-05-20 14:47:55 -0600

    5/20

---
+++ week03/scripts/place.js
@@ -0,0 +1,17 @@
+const short = document.querySelector("#short");
+const year = document.querySelector("#year")
+
+const today = new Date();
+
+const time = new Intl.DateTimeFormat("en-US", {
+    timeStyle: "medium",
+}).format(today);
+
+short.innerHTML = `Last Modification: <span class="highlight">${new Intl.DateTimeFormat(
+    "en-US",
+    {
+        dateStyle: "short"
+    }
+).format(today)}  ${time}</,span>`;
+
+year.innerHTML = `<span class="highlight">${today.getFullYear()}</span>`;
